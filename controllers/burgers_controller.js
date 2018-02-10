0eef8e74727dc36a63a55781571d1d3442555d4c
dmfriendjr <davidmfriendjr@gmail.com>
2018-02-10 18:26:57 -0500

    Seperated routes for burger api from server.js to a seperate burger_controllers file

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,9 +1,22 @@
 const express = require('express');
-const orm = require('../config/orm');
+const burger = require('../models/burger');
 
-const burgers = {
-  getAllBurgers: async () => {
-    let data = await orm.selectAll('burgers');
-    return data;
-  }
-}
+var router = express.Router()
+
+router.get('/burgers', async (req, res) => {
+  let data = await burger.getAllBurgers();
+  res.send(data);
+})
+
+router.post('/burgers', async (req, res) => {
+  let data = await burger.addBurger(req.body.burgerName);
+  res.send(data);
+})
+
+router.put('/burgers', async (req, res) => {
+  let data = await burger.updateBurger(req.body.burgerId,req.body.devoured);
+  res.send(data);
+})
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,27 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const orm = require('./config/orm');
-const burger = require('./models/burger');
+const apiRoutes = require('./controllers/burgers_controller');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-
-app.get('/api/burgers', async (req, res) => {
-  let data = await burger.getAllBurgers();
-  res.send(data);
-})
-
-app.post('/api/burgers', async (req, res) => {
-  let data = await burger.addBurger(req.body.burgerName);
-  res.send(data);
-})
-
-app.put('/api/burgers', async (req, res) => {
-  let data = await burger.updateBurger(req.body.burgerId,req.body.devoured);
-  res.send(data);
-})
-
+app.use('/api', apiRoutes);
 
 app.listen(port);
