c9e6fab539cf76c732b41e8a6afa667475883883
dmfriendjr <davidmfriendjr@gmail.com>
2018-02-10 20:05:41 -0500

    Added some basic html to test posting from site directly. Modified API responses to allow posting directly from form on the site

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,17 +6,26 @@ var router = express.Router()
 router.get('/burgers', async (req, res) => {
   let data = await burger.getAllBurgers();
   res.send(data);
-})
-
-router.post('/burgers', async (req, res) => {
-  let data = await burger.addBurger(req.body.burgerName);
-  res.send(data);
-})
+});
 
 router.put('/burgers', async (req, res) => {
   let data = await burger.updateBurger(req.body.burgerId,req.body.devoured);
   res.send(data);
-})
+});
+
+router.post('/burgers', async (req, res, next) => {
+  await burger.addBurger(req.body.burgerName);
+  renderIndex(res);
+});
+
+router.get('/', async (req, res) => {
+  renderIndex(res);
+});
+
+async function renderIndex(res) {
+  let data = await burger.getAllBurgers();
+  res.render('index', { uneatenBurgers: data });
+}
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
 const apiRoutes = require('./controllers/burgers_controller');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.use('/api', apiRoutes);
 
 app.listen(port);
