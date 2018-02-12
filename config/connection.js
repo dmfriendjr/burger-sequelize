6bb1127d9665938e46344fbb1c3caa1430c7854b
dmfriendjr <davidmfriendjr@gmail.com>
2018-02-12 14:27:55 -0500

    Reverted unworking changes, now reassigning connection variable to new connection when connection is dropped by server

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection(process.env.DATABASE_URL);
+var connection = mysql.createConnection(process.env.DATABASE_URL);
 
 connection.on('error', (err) => {
   if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-    connnection.createConnection(process.env.DATABASE_URL)
+    connection = mysql.createConnection(process.env.DATABASE_URL)
   }
 })
 
