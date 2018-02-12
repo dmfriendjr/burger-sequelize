d9f3ce929a5e6a45bc3a947c989a5d4ea0237763
dmfriendjr <davidmfriendjr@gmail.com>
2018-02-12 14:17:42 -0500

    Trying to catch server ending connection and re-establishing connection in this event

--- config/connection.js
+++ config/connection.js
@@ -2,4 +2,10 @@ const mysql = require('mysql');
 
 const connection = mysql.createConnection(process.env.DATABASE_URL);
 
+connection.on('error', (err) => {
+  if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+    connection = mysql.createConnection(process.env.DATABASE_URL);
+  }
+})
+
 module.exports = connection;
--- config/orm.js
+++ config/orm.js
@@ -2,7 +2,7 @@ const connection = require('./connection');
 
 connection.query(`USE ${process.env.DATABASE_NAME}`, (err, res) => {
   connection.query('CREATE TABLE IF NOT EXISTS burgers (id int NOT NULL AUTO_INCREMENT, burger_name VARCHAR(100) NOT NULL, devoured boolean DEFAULT false, time_stamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY(id))');
-})
+});
 
 var orm = {
   selectAll: (tableName, selectionTarget, selectionValue) => {
