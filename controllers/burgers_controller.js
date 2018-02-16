b21b4113643fd880ecf9fbcb0ec1c01472fdbc89
dmfriendjr <davidmfriendjr@gmail.com>
2018-02-16 13:12:43 -0500

    Added serverside validation for burger name length as well as handling promise rejection in event of an error

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,47 +4,88 @@ const db = require('../models');
 var router = express.Router()
 
 router.get('/api/burgers/:eaten?', async (req, res) => {
-  if (!req.params.eaten) {
-    let data = await db.burgers.findAll();
-    res.statusCode = 200;
-    res.send(data);
-  } else if (req.param.eaten == 'true') {
-    let data = await db.burgers.findAll({where: {devoured: true}});
-    res.statusCode = 200;
-    res.send(data);
-  } else {
-    let data = await db.burgers.findAll({where: {devoured: false}});
-    res.statusCode = 200;
-    res.send(data);
+  let data;
+
+  try {
+    if (!req.params.eaten) {
+        data = await db.burgers.findAll();
+    } else if (req.param.eaten == 'true') {
+        data = await db.burgers.findAll({where: {devoured: true}});
+    } else {
+        data = await db.burgers.findAll({where: {devoured: false}});
+    }
+  } catch(e) {
+    if (e.name === 'SequelizeValidationError') {
+      console.log('Validation error occured.');
+    } else {
+      throw(e);
+    }
+    res.statusCode = 400;
+    res.send();
+    return;
   }
+
+  res.statusCode = 200;
+  res.send(data);
 });
 
 router.put('/api/burgers', async (req, res) => {
-  let data = await db.burgers.update(req.body, {where: {id: req.body.id}});
+
+  let data;  
+  try {
+    data = await db.burgers.update(req.body, {where: {id: req.body.id}});
+  } catch(e) {
+    if (e.name === 'SequelizeValidationError') {
+      console.log('Validation error occured.');
+    } else {
+      throw(e);
+    }
+    res.statusCode = 400;
+    res.send();
+    return;
+  }
+
   res.statusCode = 200;
   res.send(data);
 });
 
 router.post('/api/burgers', async (req, res, next) => {
-  if (req.body.burger_name.length === 0) {
-    res.statusCode = 400;
-    res.send();
-    return;
-  }
   //Ensure burger name contains the word 'burger' for consistency
   if (req.body.burger_name.match(/(burger)/gi) === null) {
     req.body.burgerName += ' Burger';
   }
 
-  let data = await db.burgers.create(req.body);
+  let data;
+  try {
+    data = await db.burgers.create(req.body);
+  } catch(e) {
+    if (e.name === 'SequelizeValidationError') {
+      console.log('Validation error occured');
+    } else {
+      throw (e);
+    }
+    res.statusCode = 400;
+    res.send();
+    return;
+  }
 
   res.statusCode = 200;
   res.send(data);
 });
 
 router.get('/', async (req, res) => {
-  let uneatenData = await db.burgers.findAll({where: {devoured: false}});
-  let eatenData = await db.burgers.findAll({where: {devoured: true}});
+  let uneatenData;
+  let eatenData;
+
+  try {
+    uneatenData = await db.burgers.findAll({where: {devoured: false}});
+    eatenData = await db.burgers.findAll({where: {devoured: true}});
+  } catch(e) {
+    res.statusCode = 500;
+    res.send();
+    return;
+  }
+
   let uneatenBurgers = convertBurgerData(uneatenData); 
   let eatenBurgers = convertBurgerData(eatenData);
   res.render('index', { uneatenBurgers, eatenBurgers });
--- models/burgers.js
+++ models/burgers.js
@@ -1,7 +1,14 @@
 module.exports = (sequelize, DataTypes) => {
   return sequelize.define('burgers', {
-    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    burger_name: {type: DataTypes.STRING},
+    id: {
+      type: DataTypes.INTEGER, 
+      primaryKey: true, 
+      autoIncrement: true,
+    },
+    burger_name: {
+      type: DataTypes.STRING,
+      validate: { len: [1, 25] }
+    },
     devoured: {type: DataTypes.BOOLEAN, defaultValue: false}
   })
 }
